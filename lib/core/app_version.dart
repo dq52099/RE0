6d4c6cc35e6ec4f0571a9720aec3b062d4403c6a
dq52099 <1851939263@qq.com>
2026-04-29 04:36:17 +0000

    Increase release build heap

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -3,11 +3,11 @@ class AppVersion {
 
   static const name = String.fromEnvironment(
     'RE0_VERSION_NAME',
-    defaultValue: '1.1.34',
+    defaultValue: '1.1.35',
   );
 
   static const code = int.fromEnvironment(
     'RE0_VERSION_CODE',
-    defaultValue: 10134,
+    defaultValue: 10135,
   );
 }
