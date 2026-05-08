212ae039b479814bcab7877de1909ea03d460d3c
dq52099 <1851939263@qq.com>
2026-05-08 18:42:08 +0000

    Release 1.2.14

--- lib/core/app_version.dart
+++ lib/core/app_version.dart
@@ -3,16 +3,16 @@ class AppVersion {
 
   static const name = String.fromEnvironment(
     'RE0_VERSION_NAME',
-    defaultValue: '1.2.13',
+    defaultValue: '1.2.14',
   );
 
   static const code = int.fromEnvironment(
     'RE0_VERSION_CODE',
-    defaultValue: 10213,
+    defaultValue: 10214,
   );
 
   static const releaseTag = String.fromEnvironment(
     'RE0_RELEASE_TAG',
-    defaultValue: 'v1.2.13',
+    defaultValue: 'v1.2.14',
   );
 }
